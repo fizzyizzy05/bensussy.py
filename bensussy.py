8558e3d9a996d3462f49ea01a1523a1a4468e101
IJustNeedOffsets <80121524+IJustNeedOffsets@users.noreply.github.com>
2021-07-22 00:29:18 +0200

    Update bensussy.py

--- bensussy.py
+++ bensussy.py
@@ -5,10 +5,11 @@ sussy = "Ben"
 if sussy == whosussy:
     print("Yes, you are right. Ben is always sus, never not sus")
 else: 
-    whosussy != "Ben"
+    whosussy != sussy
     while True:
         print("There's literally no way on earth " + whosussy + " is sus. You're sus for saying they're sus")
         whosussy = input("Stop with the cap and tell the truth. Who is the sussy? ")
-        if whosussy == "Ben":
+        if whosussy == sussy:
             print("Finally, you admit that Ben is the sussy.")
+            print("Ben: gheuq8efkedjwjq)
             quit()
