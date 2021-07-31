9399088b5e3410fbb63f0a1c7879f50740bfdaed
Atharv Attri <48738128+Atharv-Attri@users.noreply.github.com>
2021-07-30 18:11:51 -0700

    Update bensussy.py

--- bensussy.py
+++ bensussy.py
@@ -1,14 +1,14 @@
-print("bensussy.py is a script made by jennasilicon. Don't take it seriously, it was made for an inside joke xD")
-print(" ")
-whosussy = input("Who is sussy? ")
-sussy = "Ben"
+print("bensussy.py is a script made by jennasilicon. Don't take it seriously, it was made for an inside joke xD\n")
+
+whosussy = input("Who is sussy? ").lower()
+sussy = "ben"
+
 if sussy == whosussy:
     print("Yes, you are right. Ben is always sus, never not sus")
-else: 
-    whosussy != "Ben"
-    while True:
-        print("There's literally no way on earth " + whosussy + " is sus. You're sus for saying they're sus")
-        whosussy = input("Stop with the cap and tell the truth. Who is the sussy? ")
-        if whosussy == "Ben":
-            print("Finally, you admit that Ben is the sussy.")
-            quit()
+    quit()
+
+while whosussy != "ben":
+    print(f"There's literally no way on earth {whosussy} is sus. You're sus for saying they're sus")
+    whosussy = input("Stop with the cap and tell the truth. Who is the sussy? ")
+
+print("Finally, you admit that Ben is the sussy.")
