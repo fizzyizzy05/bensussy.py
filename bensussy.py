ce72a1f96e0dae96029f534b5a3cf211627a1899
Atharv Attri <48738128+Atharv-Attri@users.noreply.github.com>
2021-07-30 18:23:04 -0700

    Update bensussy.py

--- bensussy.py
+++ bensussy.py
@@ -1,14 +1,19 @@
-print("bensussy.py is a script made by jennasilicon. Don't take it seriously, it was made for an inside joke xD\n")
+def main() -> None:
+    print("bensussy.py is a script made by jennasilicon. Don't take it seriously, it was made for an inside joke xD\n")
 
-whosussy = input("Who is sussy? ").lower()
-sussy = "ben"
+    whosussy = input("Who is sussy? ").lower()
+    sussy = "ben"
 
-if sussy == whosussy:
-    print("Yes, you are right. Ben is always sus, never not sus")
-    quit()
+    if sussy == whosussy:
+        print("Yes, you are right. Ben is always sus, never not sus")
+        return
 
-while whosussy != "ben":
-    print(f"There's literally no way on earth {whosussy} is sus. You're sus for saying they're sus")
-    whosussy = input("Stop with the cap and tell the truth. Who is the sussy? ")
+    while whosussy != sussy:
+        print(f"There's literally no way on earth {whosussy} is sus. You're sus for saying they're sus")
+        whosussy = input("Stop with the cap and tell the truth. Who is the sussy? ")
 
-print("Finally, you admit that Ben is the sussy.")
+    print("Finally, you admit that Ben is the sussy.")
+
+
+if __name__ == "__main__":
+    main()
